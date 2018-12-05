d3dbda0922fd07bc160cb05dd958e578d9a16c5d
caleb <cehrich02@gmail.com>
2018-12-05 18:13:45 -0500

    Added GetGuess method to gather user's number and store it in a list.

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -8,8 +8,24 @@ namespace MasterMind
 {
     class Program
     {
+        static List<int> userNumber = new List<int> { };
+
+
         static void Main(string[] args)
         {
+        }
+
+        public void GetGuess()
+        {
+            Console.WriteLine("Please enter four numbers between 1-6");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 4; i++)
+            {
+                userNumber.Insert(0, userGuess % 10);
+                userGuess /= 10;
+            }
+
         }
     }
 }
